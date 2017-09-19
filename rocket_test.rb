9a8d64e250074acfb49724f13a5d7ac375d25e50
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-09-19 16:34:29 -0400

    add colour and name tests

--- rocket_test.rb
+++ rocket_test.rb
@@ -2,5 +2,25 @@ require "minitest/autorun"
 require_relative "rocket"
 
 class RocketTest < Minitest::Test
-  # Write your tests here!
+
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_has_name
+    # arrange
+      @rocket = Rocket.new(name: "aries")
+  # act
+    result = @rocket.name   # expect it
+#assert#
+    assert_equal("aries",result)
+  end
+
+  def test_has_colour
+    @rocket = Rocket.new(colour: "red")
+    result = @rocket.colour
+    assert_equal("red",result)
+  end
+
+
 end
